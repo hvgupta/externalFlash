6f0dd00d619c91e42b2f2c6edaf51e2a50f0bf29
unknown <zguobd@connect.ust.hk>
2024-01-03 01:20:32 +0800

    feat(Template): Generate success and compile

--- Core/Inc/AppConfig.h
+++ Core/Inc/AppConfig.h
@@ -66,12 +66,12 @@
 /*====================
    POWER CONTROLLER
  *====================*/
-#define USE_POWER_CONTROLLER 0
+#define USE_POWER_CONTROLLER 1
 
 /*====================
    SUPER CAPACITOR
  =====================*/
-#define USE_SUPER_CAPACITOR 0
+#define USE_SUPER_CAPACITOR 1
 
 /*====================
    PID CONFIG
@@ -103,7 +103,7 @@
 /*====================
    DM4310 MOTOR CONFIG
  *====================*/
- #define USE_DM4310 0
+ #define USE_DM4310 1
  #if USE_DM4310
 /*4310 id setting, must be the same as configrator*/
     #define DM4310_ID_CUSTOM 0
@@ -127,7 +127,7 @@
 /*====================
    DR16 CONFIG
  *====================*/
-#define USE_DR16 0
+#define USE_DR16 1
 #if USE_DR16
     #define USE_DR16_DMA 1
     #define USE_DR16_INTERRUPT 0
@@ -144,14 +144,11 @@
 /*====================
    FDCAN CONFIG
  *====================*/
-#define USE_CAN_MANAGER 0
-#ifdef USE_CAN_MANAGER
 #define CAN_CUSTOM 0
 #if CAN_CUSTOM
     #define CAN_NUM 1
     #define CAN_FILTER_NUM 8
     #define CAN_FILTER_SLAVE_START 14
-#endif
 #endif
 
 /*====================
@@ -187,7 +184,7 @@
 /*====================
    REFEREE SYSTEM COMM
  *====================*/
-#define USE_REFEREE_SYSTEM_COMM 0
+#define USE_REFEREE_SYSTEM_COMM 1
 #if USE_REFEREE_SYSTEM_COMM
     #define REFEREE_BUFFER_SIZE 150
     #define REFEREE_RX_LIST_SIZE 64
@@ -198,12 +195,12 @@
 /*====================
    ROS COMM CONFIG
  *====================*/
-#define USE_ROS_COMM 0
+#define USE_ROS_COMM 1
 #if USE_ROS_COMM
-#define ROS_COMM_UART huart2
-#define ROS_PROTOCOL_CUSTOM 0
+#define ROS_COMM_UART huart5
+#define ROS_PROTOCOL_CUSTOM 1
 #if ROS_PROTOCOL_CUSTOM
-    #define ROS_BUF_SIZE 32
+    #define ROS_BUF_SIZE 60
     #define ROS_TX_QUEUE_LENGTH 16
     #define ROS_TX_TIMEOUT 10
     #define ROS_RX_TIMEOUT 50
--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -7,7 +7,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
+  * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -27,7 +27,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f4xx_hal.h"
+#include "stm32g4xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -57,15 +57,68 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define ICM_RDY_Pin GPIO_PIN_1
-#define ICM_RDY_GPIO_Port GPIOC
-#define ICM_RDY_EXTI_IRQn EXTI1_IRQn
-#define LED0_Pin GPIO_PIN_0
-#define LED0_GPIO_Port GPIOB
-#define LASER_Pin GPIO_PIN_1
-#define LASER_GPIO_Port GPIOB
-#define ICM_CS_Pin GPIO_PIN_9
-#define ICM_CS_GPIO_Port GPIOB
+#define LED_ACT_Pin GPIO_PIN_13
+#define LED_ACT_GPIO_Port GPIOC
+#define LASER_Pin GPIO_PIN_14
+#define LASER_GPIO_Port GPIOC
+#define BUTTON_Pin GPIO_PIN_15
+#define BUTTON_GPIO_Port GPIOC
+#define NRST_Pin GPIO_PIN_10
+#define NRST_GPIO_Port GPIOG
+#define UART2A_CTS_Pin GPIO_PIN_0
+#define UART2A_CTS_GPIO_Port GPIOA
+#define UART2A_RTS_Pin GPIO_PIN_1
+#define UART2A_RTS_GPIO_Port GPIOA
+#define UART2A_TX_Pin GPIO_PIN_2
+#define UART2A_TX_GPIO_Port GPIOA
+#define UART2A_RX_Pin GPIO_PIN_3
+#define UART2A_RX_GPIO_Port GPIOA
+#define CS_IMU_Pin GPIO_PIN_4
+#define CS_IMU_GPIO_Port GPIOA
+#define UART1A_TX_Pin GPIO_PIN_4
+#define UART1A_TX_GPIO_Port GPIOC
+#define UART1A_RX_Pin GPIO_PIN_5
+#define UART1A_RX_GPIO_Port GPIOC
+#define DRDY_MAG_Pin GPIO_PIN_0
+#define DRDY_MAG_GPIO_Port GPIOB
+#define DRDY_IMU_Pin GPIO_PIN_1
+#define DRDY_IMU_GPIO_Port GPIOB
+#define CS_MAG_Pin GPIO_PIN_2
+#define CS_MAG_GPIO_Port GPIOB
+#define IMU_HEAT_Pin GPIO_PIN_7
+#define IMU_HEAT_GPIO_Port GPIOE
+#define UART3B_RX_Pin GPIO_PIN_15
+#define UART3B_RX_GPIO_Port GPIOE
+#define UART3B_TX_Pin GPIO_PIN_10
+#define UART3B_TX_GPIO_Port GPIOB
+#define UART3A_TX_Pin GPIO_PIN_8
+#define UART3A_TX_GPIO_Port GPIOD
+#define UART3A_RX_Pin GPIO_PIN_9
+#define UART3A_RX_GPIO_Port GPIOD
+#define UART4_DE_Pin GPIO_PIN_15
+#define UART4_DE_GPIO_Port GPIOA
+#define UART4_TX_Pin GPIO_PIN_10
+#define UART4_TX_GPIO_Port GPIOC
+#define UART4_RX_Pin GPIO_PIN_11
+#define UART4_RX_GPIO_Port GPIOC
+#define UART5_TX_Pin GPIO_PIN_12
+#define UART5_TX_GPIO_Port GPIOC
+#define UART5_RX_Pin GPIO_PIN_2
+#define UART5_RX_GPIO_Port GPIOD
+#define UART2B_TX_Pin GPIO_PIN_5
+#define UART2B_TX_GPIO_Port GPIOD
+#define UART2B_RX_Pin GPIO_PIN_6
+#define UART2B_RX_GPIO_Port GPIOD
+#define UART1B_TX_Pin GPIO_PIN_6
+#define UART1B_TX_GPIO_Port GPIOB
+#define UART1B_RX_Pin GPIO_PIN_7
+#define UART1B_RX_GPIO_Port GPIOB
+#define BOOT0_Pin GPIO_PIN_8
+#define BOOT0_GPIO_Port GPIOB
+#define UART1C_TX_Pin GPIO_PIN_0
+#define UART1C_TX_GPIO_Port GPIOE
+#define UART1C_RX_Pin GPIO_PIN_1
+#define UART1C_RX_GPIO_Port GPIOE
 
 /* USER CODE BEGIN Private defines */
 
